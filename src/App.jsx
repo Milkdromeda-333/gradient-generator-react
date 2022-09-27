c37fd6d6dc4f93c950a2f24a32a9021fcf9956de
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-09-26 20:24:38 -0700

    finished MVP. added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,34 @@ import { useState } from "react";
 /*
 Project: build a gradient generator that outputs generated css gradient for multiple browswers. form takes in colors, and angle, and direction.
 IDEA: 
-
--  gradient will load with a preset gradient.
-- inputs: 
-  - colors (optional to add more)
-  - direction
-
-  once the user submits the inputs , the textarea presents the css.
+TODO:
+-[]add option of additional colors
+-[]add ability to view saved gradients
 
 */
 function App() {
-  const [gradient, setGradient] = useState("linear-gradient(to bottom right, red, yellow)");
+  const [gradient, setGradient] = useState("linear-gradient(to bottom right, #ff0000, #ffff00)");
+
+  const [inputsObj, setInputsObj] = useState({
+    color1: "#ff0000",
+    color2: "#ffff00",
+    direction: "to bottom right"
+  });
+
+  // takes the input an sets gradient to returned input
+  function handleSubmit(e) {
+    e.preventDefault();
+    setGradient(`linear-gradient(${inputsObj.direction}, ${inputsObj.color1}, ${inputsObj.color2})`);
+  }
+
+  // updates inputs in form
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInputsObj(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
 
   return (
     <div>
@@ -22,32 +39,32 @@ function App() {
       </header>
       <main>
         <section className="gradient">
-          <div className="gradient--viewer" style={{ backgroundImage: gradient }}></div>
-          <textarea readOnly></textarea>
+          <div className="gradient--viewer" style={{ background: gradient }}></div>
+          <textarea readOnly value={gradient} />
         </section>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="color1">
             Color 1:
-            <input type="color" name="color1" id="color1" />
+            <input type="color" name="color1" id="color1" onChange={handleChange} />
           </label>
 
           <label htmlFor="color2">
             Color 2:
-            <input type="color" name="color2" id="color2" />
+            <input type="color" name="color2" id="color2" onChange={handleChange} />
           </label>
 
-          <select>
-            <option value="top-to-bottom">Top to bottom</option>
-            <option value="left-to-right">Left to right</option>
-            <option value="top-left-to-bottom-right">Top left to bottom right</option>
+          <select name="direction" onChange={handleChange}>
+            <option value="top to bottom">Top to bottom</option>
+            <option value="to right">Left to right</option>
+            <option value="to bottom right">Top left to bottom right</option>
           </select>
 
           <button>submit</button>
         </form>
       </main>
-    </div>
+    </div >
   );
 }
 
-export default App;
+export default App;;
